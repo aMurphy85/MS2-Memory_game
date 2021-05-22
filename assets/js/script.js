ee124d0175f7e3b88acd4acad4996ff0f6595796
aMurphy85 <murv121@hotmail.co.uk>
2021-05-22 13:32:46 +0000

    Add images to be used in memory game, add array of cards in script.js, add basic css styling for game area

---
+++ assets/js/script.js
@@ -0,0 +1,28 @@
+// Game cards array
+
+const cardsArray = [
+    {
+        name: 'brown Crewmate', img: 'assets/imgs/brownCrewmate.png',
+    },
+    {
+        name: 'dead Crewmate', img: 'assets/imgs/deadCrewmate.png',
+    },
+    {
+        name: 'green Crewmate', img: 'assets/imgs/greenCrewmate.png',
+    },
+    {
+        name: 'lime Crewmate', img: 'assets/imgs/limeCrewmate.png',
+    },
+    {
+        name: 'orange Crewmate', img: 'assets/imgs/orangeCrewmate.png',
+    },
+    {
+        name: 'pink Crewmate', img: 'assets/imgs/pinkCrewmate.png',
+    },
+    {
+        name: 'red Crewmate', img: 'assets/imgs/redCrewmate.png',
+    },
+    {
+        name: 'teal Crewmate', img: 'assets/imgs/tealCrewmate.png',
+    },
+]
